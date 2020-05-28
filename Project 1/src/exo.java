b36082d2671200ab0246c22382f3aa1dd57bb9f4
Aurel-GitHub <56591961+Aurel-GitHub@users.noreply.github.com>
2020-05-28 13:59:41 +0200

    Add files via upload

---
+++ Project 1/src/exo.java
@@ -0,0 +1,23 @@
+public class exo {
+
+	public static void main(int[] args) {
+		/**
+		 * nom du film
+		 * je l'ai vu ?
+		 * anne du film 
+		 * note du film
+		 */
+		String titre = "Indiana chez les tanges";
+		boolean vue = true;
+		int annee = 2000;
+		double note = 8.2;
+		System.out.println("le film " + titre );
+		System.out.println("est sortie le " + annee );
+		System.out.println("a eu la note de " + note );
+		System.out.println("a été vu " + vue);
+
+
+
+	}
+
+}
